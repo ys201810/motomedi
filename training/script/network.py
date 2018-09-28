ded95aa952bfbc117ef1fe607b17531ae4b6421d
ys201810 <ys.201810@gmail.com>
2018-09-28 16:58:20 +0900

    [fix] model code separate from training
    delete network code from train.py.
    create new file network.py

---
+++ training/script/network.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*- 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization
+
+
+def easy_model(input_shape, num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax'))
+    return model
+
+
+def darknet19(input_shape, num_classes):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    model.add(Conv2D(64, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    model.add(Conv2D(128, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(64, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(128, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    model.add(Conv2D(256, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(128, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(256, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(256, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(256, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+
+    model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(512, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    # model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
+    # model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Conv2D(512, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
+    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    # model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape, name='bb'))
+    # model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    # for layer in model.layers:
+        # print(layer.get_output_at(0).get_shape().as_list())
+    return model
--- training/script/train.py
+++ training/script/train.py
@@ -8,94 +8,9 @@ from keras.preprocessing import image
 from sklearn.model_selection import train_test_split
 import keras
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, BatchNormalization
 from keras import optimizers
+import network
 
-
-def model1(input_shape, num_classes):
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
-    return model
-
-
-def model2(input_shape, num_classes):
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
-    model.add(Conv2D(64, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
-    model.add(Conv2D(128, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(64, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(128, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
-    model.add(Conv2D(256, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(128, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(256, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
-    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(256, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(256, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(512, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
-    model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(512, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    # model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape))
-    # model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Conv2D(512, kernel_size=(1, 1),activation='relu',input_shape=input_shape))
-    model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    # model.add(Conv2D(1024, kernel_size=(3, 3),activation='relu',input_shape=input_shape, name='bb'))
-    # model.add(BatchNormalization(momentum=0.99, epsilon=0.001, center=True, scale=True))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dense(num_classes, activation='softmax'))
-
-    for layer in model.layers:
-        print(layer.get_output_at(0).get_shape().as_list())
-    return model
 
 def main():
     os.environ['PYTHONHASHSEED'] = '0'
@@ -134,7 +49,7 @@ def main():
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state= 3)
 
     print('model load')
-    model = model2(input_shape, num_classes)
+    model = network.darknet19(input_shape, num_classes)
     sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
 
     model.compile(loss=keras.losses.categorical_crossentropy,
