f57b9b51ac3b9cfda4cec0a663ca6339b66ee998
ys201810 <ys.201810@gmail.com>
2018-11-04 22:32:39 +0900

    [add] deep auto auto_encoder

--- training/script/autoencoder/auto_encoder_network.py
+++ training/script/autoencoder/auto_encoder_network.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 
-from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
 
 def simple_auto_encoder(t_conf):
@@ -16,22 +16,25 @@ def simple_auto_encoder(t_conf):
 
 
 def deep_auto_encoder(t_conf):
-    input_img = Input(shape=(1, t_conf.image_size[0], t_conf.image_size[1]))
+    # Input shape = W, H, C
+    # input_img = Input(shape=(t_conf.image_size[2], t_conf.image_size[0], t_conf.image_size[1]))
+    input_img = Input(shape=(t_conf.image_size[0], t_conf.image_size[1], t_conf.image_size[2]))
 
-    x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(input_img)
-    x = MaxPooling2D((2, 2), border_mode='same')(x)
-    x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-    x = MaxPooling2D((2, 2), border_mode='same')(x)
-    x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-    encoded = MaxPooling2D((2, 2), border_mode='same')(x)
+    x = Conv2D(filters=16, kernel_size=(3, 3), strides=1, activation='relu', padding='same', name='conv1')(input_img)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2, name='pool1')(x)
+    x = Conv2D(filters=8, kernel_size=(3, 3), strides=1, activation='relu', padding='same', name='conv2')(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2, name='pool2')(x)
+    x = Conv2D(filters=8, kernel_size=(3, 3), strides=1, activation='relu', padding='same', name='conv3')(x)
+    encoded = MaxPooling2D(pool_size=(2, 2), strides=2, padding='same', name='pool3')(x)
 
-    x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(encoded)
-    x = UpSampling2D((2, 2))(x)
-    x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Convolution2D(16, 3, 3, activation='relu')(x)
-    x = UpSampling2D((2, 2))(x)
-    decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+    x = Conv2D(filters=8, kernel_size=(3, 3), strides=1, activation='relu', padding='same', name='conv4')(encoded)
+    x = UpSampling2D((2, 2), name='upsa1')(x)
+    x = Conv2D(filters=8, kernel_size=(3, 3), strides=1, activation='relu', padding='same', name='conv5')(x)
+    x = UpSampling2D((2, 2), name='upsa2')(x)
+    x = Conv2D(filters=16, kernel_size=(3, 3), strides=1, activation='relu', name='conv6')(x)
+    x = UpSampling2D((2, 2), name='upsa3')(x)
+    # decode's fileters correspond with channel size
+    decoded = Conv2D(filters=1, kernel_size=(3, 3), strides=1, activation='sigmoid', padding='same', name='conv7')(x)
 
     autoencoder = Model(input_img, decoded)
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
